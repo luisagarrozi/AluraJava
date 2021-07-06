9df44ede58791eb33d6cef2a755a23b8014b3fa7
luisaoliveira <luisagarrozi@gmail.com>
2021-07-06 17:05:14 -0300

    Criando clientes

--- src/conta.java
+++ src/conta.java
@@ -2,9 +2,26 @@ public class conta {
     double saldo;
     int agencia;
     int numero;
-    String titular;
+    Cliente titular;
 
     public void depositar(double valor) {
         this.saldo = this.saldo + valor;
+    }
+
+    public boolean sacar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean transferir(double valor, conta destino){
+        if (this.saldo >= valor){
+            this.saldo -= valor;
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
     }
 }
--- src/criaConta.java
+++
@@ -1,7 +0,0 @@
-public class criaConta {
-    public static void main(String[] args) {
-        conta PrimeiraConta = new conta();
-        PrimeiraConta.saldo = 200;
-        System.out.println(PrimeiraConta.saldo);
-    }
-}
