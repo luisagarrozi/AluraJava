2ffc6d772e585da547d717d7e3715f5f5ffff7bc
luisaoliveira <luisagarrozi@gmail.com>
2021-07-06 22:05:20 -0300

    Banco Encapsulado

--- src/Métodos.java
+++ src/Métodos.java
@@ -4,14 +4,13 @@ public class Métodos {
 
     public static void main(String[] args) {
         conta Minhaconta = new conta();
-        Minhaconta.saldo = 100;
         // Depositar dinheiro
-        Minhaconta.depositar(50);
-        System.out.println("Valor inicial da sua conta é " + Minhaconta.saldo);
+        Minhaconta.depositar(150);
+        System.out.println("Valor inicial da sua conta é " + Minhaconta.getSaldo());
 
         // Sacar dinheiro
         boolean Retirada = Minhaconta.sacar(20);
-        System.out.println("Valor da conta após saque é " + Minhaconta.saldo);
+        System.out.println("Valor da conta após saque é " + Minhaconta.getSaldo());
 
         // Criar outra conta para fazer transferência
         conta Contaoutros = new conta();
@@ -23,8 +22,8 @@ public class Métodos {
         } else {
             System.out.println("Transferência falhou por falta de dinheiro");
         }
-        System.out.println("Saldo da conta de saída " + Contaoutros.saldo);
-        System.out.println("Saldo da conta de entrada " + Minhaconta.saldo);
+        System.out.println("Saldo da conta de saída " + Contaoutros.getSaldo());
+        System.out.println("Saldo da conta de entrada " + Minhaconta.getSaldo());
 
         // Novo Cliente
         Cliente Luisa = new Cliente();
@@ -33,7 +32,7 @@ public class Métodos {
         Luisa.profissão = "estudante";
 
         // Associa a conta a um cliente
-        Minhaconta.titular = Luisa;
+        Minhaconta.setTitular(Luisa);
         System.out.println("Essa é a conta de " + Luisa.nome);
     }
 }
--- src/conta.java
+++ src/conta.java
@@ -1,8 +1,36 @@
 public class conta {
-    double saldo;
-    int agencia;
-    int numero;
-    Cliente titular;
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
 
     public void depositar(double valor) {
         this.saldo = this.saldo + valor;
@@ -16,8 +44,9 @@ public class conta {
             return false;
         }
     }
-    public boolean transferir(double valor, conta destino){
-        if (this.saldo >= valor){
+
+    public boolean transferir(double valor, conta destino) {
+        if (this.saldo >= valor) {
             this.saldo -= valor;
             destino.depositar(valor);
             return true;
